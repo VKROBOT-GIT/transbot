2d9bf01b17b4c8eb669dd4cd1a5db97cd0d57d56
fjpaxkm <jpfeng@foxmail.com>
2020-10-27 16:56:49 +0800

    update code

--- startup/create_udev_rules.sh
+++ startup/create_udev_rules.sh
@@ -3,7 +3,7 @@
 echo "remap the device serial port(ttyUSBX) to  vk-xxx"
 echo "Transbot usb connection as /dev/vk-xxx , check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy xxxx.rules to  /etc/udev/rules.d/"
-sudo cp ~/transbot_ws/startup/rules.d/transbot.rules  /etc/udev/rules.d
+sudo cp ~/transbot_ws/src/transbot/startup/rules.d/transbot.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
