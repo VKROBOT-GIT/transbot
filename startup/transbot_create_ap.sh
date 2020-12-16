ecdd03773cdfb7f487c85d5fbf58f12b15c5ef26
fjpaxkm <jpfeng@foxmail.com>
2020-12-16 13:55:00 +0800

    commit code
    commit code

--- startup/transbot_create_ap.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-gnome-terminal --geometry=80x10+350+0 --title="ROS transbot creat_ap" -e "bash -c \"echo vktransbot |sudo -S create_ap -n wlan0 Transbot_ap vktransbot ; read \""     #start transbot wifi ap
