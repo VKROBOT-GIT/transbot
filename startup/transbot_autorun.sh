cd6e42440db07b62d154fa6e813b234264dc18ba
fjpaxkm <jpfeng@foxmail.com>
2020-12-16 15:27:25 +0800

    commit code

--- startup/transbot_autorun.sh
+++ startup/transbot_autorun.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 source /opt/ros/melodic/setup.bash
-source /home/vktransbot/transbot_ws/devel/setup.bash
-source /home/vktransbot/.bashrc
+source /home/transbot/transbot_ws/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.1.100:11311
 export ROS_HOSTNAME=192.168.1.100
 
-gnome-terminal --geometry=80x10+350+0 --title="ROS turtlebot3 robot" -x roslaunch turtlebot3_bringup turtlebot3_robot.launch   #start turtlebot3
+gnome-terminal --geometry=80x10+350+0 --title="ROS CORE" -x roslaunch turtlebot3_bringup transbot_robot.launch   #start transbot
+gnome-terminal --geometry=80x10+350+0 --title="ROS Transbot Bringup" -x roslaunch turtlebot3_bringup transbot_robot.launch   #start transbot bringup
+gnome-terminal --geometry=80x10+350+0 --title="ROS Transbot SLAM" -x roslaunch turtlebotslam transbot_slam.launch   #start transbot slam
 
 
